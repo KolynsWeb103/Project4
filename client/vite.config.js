58c02bfbd9bd632af7f0da84c1230f0793b393d1
Kolyn090 <kolynlin@protonmail.com>
2026-06-25 14:35:19 -0400

    Setup client side styles

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
@@ -15,9 +14,12 @@ export default defineConfig({
     }
   },
   server: {
+    host: 'localhost',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000'
+        target: 'http://localhost:3000',
+        changeOrigin: true
       }
     }
   }
